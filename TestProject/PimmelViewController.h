ec3d50d2117ec980057a92689167b14d492bdb30
thrax130 <edmund@senkleiter.de>
2014-02-26 18:05:24 +0100

    Pimmel VC added

---
+++ TestProject/PimmelViewController.h
@@ -0,0 +1,13 @@
+//
+//  PimmelViewController.h
+//  TestProject
+//
+//  Created by Edmund Senkleiter on 26.02.14.
+//  Copyright (c) 2014 SenkDev Studios 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PimmelViewController : UIViewController
+
+@end
---
+++ TestProject/PimmelViewController.m
@@ -0,0 +1,38 @@
+//
+//  PimmelViewController.m
+//  TestProject
+//
+//  Created by Edmund Senkleiter on 26.02.14.
+//  Copyright (c) 2014 SenkDev Studios 2014. All rights reserved.
+//
+
+#import "PimmelViewController.h"
+
+@interface PimmelViewController ()
+
+@end
+
+@implementation PimmelViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
